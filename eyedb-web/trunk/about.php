205a54d38f151ea2ca72e1d081c01c081ba9d1b2
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-09 22:08:00 +0000

    In progress...

--- eyedb-web/trunk/about.php
+++ eyedb-web/trunk/about.php
@@ -4,8 +4,25 @@
 <div id="CenterBlock">
 <h1 id="PageTitle">About EyeDB</h1>
 <p>
-bla bla bla
+EYEDB is a very efficient and reliable OODBMS, providing all standard OODBMS features.
 </p>
+
+<p>It is based on a client/server model. It provides transactional and concurrential services.</p>
+
+<p>Its object model is generic; classes can be manipulated as object; it supports type polymorphism; it supports template-based collections such as set, bag and array, multi-dimensional and variable dimensional arrays.</p>
+
+<p>It provides complete C++ and Java bindings, a query language based on the ODMG OQL standard, an object definition language based on the ODMG ODL, a C++ and Java EYEDB ODL generator.</p>
+
+<p>All standard database constraint features are supported: one to one, one to many and many to many relationships, unique, not null and collection cardinality constraints.</p>
+
+<p>Methods can be plugged within databases, and it provides services for trigger actions.</p>
+
+<p>It provides also security controls at database and object level.</p>
+
+<p>Multi-database objects and multi-volumes databases are supported.</p>
+
+<p>At last, it provides a generic primitive web-based tool to browse any database.</p>
+
 </div>
 
 <? include( 'footer.php'); ?>
