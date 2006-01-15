259d870c4758774a79545f360d3dce539c06b50b
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-15 22:39:17 +0000

    Added PHP code to read RSS feed and integrate it in the first page, using a cache.

--- eyedb-web/trunk/index.php
+++ eyedb-web/trunk/index.php
@@ -1,3 +1,4 @@
+<? include( 'functions.php'); ?>
 <? include( 'header.php'); ?>
 <? include( 'nav.php'); ?>
 
@@ -5,6 +6,12 @@
 <p>
 bla bla bla
 </p>
+
+<p>
+<? includeNews( 'http://blog.eyedb.org/?feed=rss2', 'news_cache_blog.eyedb.org_2.html', 60); ?>
+<? includeNews( 'http://sourceforge.net/export/rss2_projnews.php?group_id=127988', 'news_cache_sourceforge.net_news.html', 60); ?>
+</p>
+
 </div>
 
 <? include( 'footer.php'); ?>
--- eyedb-web/trunk/mailinglists.php
+++ eyedb-web/trunk/mailinglists.php
@@ -4,7 +4,55 @@
 <div id="CenterBlock">
 <h1 id="PageTitle">Mailing lists</h1>
 <p>
-bla bla bla
+Following is a list of EyeDB mailing lists.
+</p>
+
+<p>
+To subscribe to a list, visit the list info page given for each list.
+</p>
+
+<p>
+<table width="100%" border=0 padding=0>
+<tr valign="top"  style="background-color: #F0F0F0">
+<td>
+eyedb-announce@lists.sourceforge.net
+</td>
+<td>
+Announcement list for EyeDB. Low traffic. Subscription is recommended.<br>
+List&nbsp;info&nbsp;page:&nbsp;<a href="http://lists.sourceforge.net/lists/listinfo/eyedb-announce">http://lists.sourceforge.net/lists/listinfo/eyedb-announce</a><br>
+List&nbsp;archive:&nbsp;<a href="http://sourceforge.net/mailarchive/forum.php?forum=eyedb-announce">http://sourceforge.net/mailarchive/forum.php?forum=eyedb-announce</a>
+</td>
+</tr>
+<tr valign="top"  style="background-color: #F0F0F0">
+<td>
+eyedb-general@lists.sourceforge.net
+</td>
+<td>
+General discussion list for EyeDB users.<br>
+List&nbsp;info&nbsp;page:&nbsp;<a href="http://lists.sourceforge.net/lists/listinfo/eyedb-general">http://lists.sourceforge.net/lists/listinfo/eyedb-general</a><br>
+List&nbsp;archive:&nbsp;<a href="http://sourceforge.net/mailarchive/forum.php?forum=eyedb-general">http://sourceforge.net/mailarchive/forum.php?forum=eyedb-general</a>
+</td>
+</tr>
+<tr valign="top" style="background-color: #F0F0F0">
+<td>
+eyedb-devel@lists.sourceforge.net
+</td>
+<td>
+Development discussion list for EyeDB.<br>
+List&nbsp;info&nbsp;page:&nbsp;<a href="http://lists.sourceforge.net/lists/listinfo/eyedb-devel">http://lists.sourceforge.net/lists/listinfo/eyedb-devel</a><br>
+List&nbsp;archive:&nbsp;<a href="http://sourceforge.net/mailarchive/forum.php?forum=eyedb-devel">http://sourceforge.net/mailarchive/forum.php?forum=eyedb-devel</a>
+</td>
+</tr>
+<tr valign="top" style="background-color: #F0F0F0">
+<td>eyedb-cvs@lists.sourceforge.net
+</td>
+<td>
+Cvs commit notification list for EyeDB.<br>
+List&nbsp;info&nbsp;page:&nbsp;<a href="http://lists.sourceforge.net/lists/listinfo/eyedb-cvs">http://lists.sourceforge.net/lists/listinfo/eyedb-cvs</a><br>
+List&nbsp;archive:&nbsp;<a href="http://sourceforge.net/mailarchive/forum.php?forum=eyedb-cvs">http://sourceforge.net/mailarchive/forum.php?forum=eyedb-cvs</a>
+</td>
+</tr>
+</table>
 </p>
 </div>
 
