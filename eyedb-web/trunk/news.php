9c39f8f60d6e1305ecd7f894ea32391aaab07197
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-28 23:34:02 +0000

    improved image logo

--- eyedb-web/trunk/news.php
+++ eyedb-web/trunk/news.php
@@ -1,7 +1,6 @@
 </td>
 
 <td id="NewsCol">
-<div id="News">
 <p>
 <span class="NewsFrom"><a href="http://sourceforge.net/news/?group_id=127988">Sourceforge</a> news:</span><br/>
 <? includeRSS( 'http://sourceforge.net/export/rss2_projnews.php?group_id=127988', 'sourceforge_news.html', 30, 'printNews'); ?>
@@ -10,7 +9,6 @@
 <span class="NewsFrom"><a href="http://blog.eyedb.org">EyEDB blog</a> news:</span><br/>
 <? includeRSS( 'http://blog.eyedb.org/?feed=rss2', 'blog_news.html', 60, 'printNews'); ?>
 </p>
-</div>
 </td>
 <?
 $rightColumnDone = true;
