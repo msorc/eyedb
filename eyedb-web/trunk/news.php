991bb5b98d9b28823e5a0dfbbf1349cd2aee1ad5
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-28 22:47:05 +0000

    update

--- eyedb-web/trunk/news.php
+++ eyedb-web/trunk/news.php
@@ -1,17 +1,15 @@
 </td>
 
 <td id="NewsCol">
-<div id="NewsBlock">
-> LATEST NEWS 
-<div class="News">
-From <a href="http://sourceforge.net/news/?group_id=127988">Sourceforge</a>:
+<div id="News">
+<p>
+<span class="NewsFrom"><a href="http://sourceforge.net/news/?group_id=127988">Sourceforge</a> news:</span><br/>
 <? includeRSS( 'http://sourceforge.net/export/rss2_projnews.php?group_id=127988', 'sourceforge_news.html', 30, 'printNews'); ?>
-</div>
-
-<div class="News">
-From <a href="http://blog.eyedb.org">EyEDB blog</a>:
+</p>
+<p>
+<span class="NewsFrom"><a href="http://blog.eyedb.org">EyEDB blog</a> news:</span><br/>
 <? includeRSS( 'http://blog.eyedb.org/?feed=rss2', 'blog_news.html', 60, 'printNews'); ?>
-</div>
+</p>
 </div>
 </td>
 <?
