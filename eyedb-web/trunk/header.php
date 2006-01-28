a2f5d41d9c59b9693574c6177834c9a96981165e
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-28 18:47:02 +0000

    Styles update

--- eyedb-web/trunk/header.php
+++ eyedb-web/trunk/header.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-<table id="Container">
+<table id="Container" cellspacing=0 cellpadding=0;>
 
 <tr id="HeaderRow">
 <td id="HeaderCol" colspan=3>
@@ -20,22 +20,23 @@
 </tr>
 <tr id="CenterRow">
 <td id="NavCol">
-
-<ul id="Nav">
-<li><a href="keyfeatures.php">Key features</a></li>
-<li><a href="quicktour.php">Quick tour</a></li>
-<li><a href="history.php">History</a></li>
-<li><a href="platforms.php">Platforms</a></li>
-<li><a href="licensing.php">Licensing</a></li>
-<li><a href="devteam.php">Development team</a></li>
-<li><a href="mailinglists.php">Mailing Lists</a></li>
-<li><a href="http://sourceforge.net/projects/eyedb">Sourceforge</a></li>
-<li><a href="http://wiki.eyedb.org">Wiki</a></li>
-<li><a href="http://blog.eyedb.org">Blog</a></li>
-<li><a href="irc.php">IRC</a></li>
-<li><a href="http://doc.eyedb.org">Documentation</a></li>
-<li><a href="links.php">Links</a></li>
+<div id="Nav">
+<ul>
+<li>&raquo; <a href="keyfeatures.php">Key features</a></li>
+<li>&raquo; <a href="quicktour.php">Quick tour</a></li>
+<li>&raquo; <a href="history.php">History</a></li>
+<li>&raquo; <a href="platforms.php">Platforms</a></li>
+<li>&raquo; <a href="licensing.php">Licensing</a></li>
+<li>&raquo; <a href="devteam.php">Development team</a></li>
+<li>&raquo; <a href="mailinglists.php">Mailing Lists</a></li>
+<li>&raquo; <a href="http://sourceforge.net/projects/eyedb">Sourceforge</a></li>
+<li>&raquo; <a href="http://wiki.eyedb.org">Wiki</a></li>
+<li>&raquo; <a href="http://blog.eyedb.org">Blog</a></li>
+<li>&raquo; <a href="irc.php">IRC</a></li>
+<li>&raquo; <a href="http://doc.eyedb.org">Documentation</a></li>
+<li>&raquo; <a href="links.php">Links</a></li>
 </ul>
+</div>
 
 <div id="Download">
 links to sourceforge
