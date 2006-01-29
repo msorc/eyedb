84175b9bd4fc90ce754a6d726565ff803e2fb1fd
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-29 11:56:29 +0000

    look++

--- eyedb-web/trunk/news.php
+++ eyedb-web/trunk/news.php
@@ -3,11 +3,11 @@
 <td id="NewsCol">
 <p>
 <span class="NewsFrom"><a href="http://sourceforge.net/news/?group_id=127988">Sourceforge</a> news:</span><br/>
-<? includeRSS( 'http://sourceforge.net/export/rss2_projnews.php?group_id=127988', 'sourceforge_news.html', 30, 'printNews'); ?>
+<? includeRSS( 'http://sourceforge.net/export/rss2_projnews.php?group_id=127988', 'sourceforge_news.html', 2*60, 'printNews'); ?>
 </p>
 <p>
 <span class="NewsFrom"><a href="http://blog.eyedb.org">EyeDB blog</a> news:</span><br/>
-<? includeRSS( 'http://blog.eyedb.org/?feed=rss2', 'blog_news.html', 60, 'printNews'); ?>
+<? includeRSS( 'http://blog.eyedb.org/?feed=rss2', 'blog_news.html', 60*60, 'printNews'); ?>
 </p>
 </td>
 <?
