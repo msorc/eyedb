7038c82818f722c46122c3a8e79700ddaff739f8
eviara <eviara@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-29 11:35:15 +0000

    Minor changes.

--- eyedb-web/trunk/history.php
+++ eyedb-web/trunk/history.php
@@ -2,7 +2,7 @@
 
 <h1>History</h1>
 
-<p>A first prototype, IDB, was developed at Genethon laboratories for the Genome View project.</p>
+<p>A first prototype, IDB, was developed at <a href="http://www.genethon.fr">Genethon laboratories</a> for the Genome View project.</p>
 
 <p>The Genome View project was initiated in 1992 to store and facilitate access to the human genome data produced at Genethon (physical and genetic maps).</p>
 
@@ -11,5 +11,16 @@
 <p>Since April 1994, a new version has being developed at SYSRA with partial fundings from the ANVAR (Agence Nationale pour la Valorisation de la Recherche), the Conseil Regional d'Ile de France and the European Contract EC BIO4-CT96-0346.</p>
 
 <p>This second version of EyeDB, is a complete rewrite. </p>
+
+<p>Since 1994, EyeDB has been used in a lot of bioinformatics projects
+in collaboration with the <a href="http://www.infobiogen.fr">
+CRI Infobiogen</a> and european projects
+with the <a href="http://www.ebi.ac.uk">EBI</a>.
+</p>
+
+<p>
+Here is the list of the <a href="publis.php">publications</a> related to
+EyeDB.
+<p/>
 
 <? include( 'footer.php'); ?>
