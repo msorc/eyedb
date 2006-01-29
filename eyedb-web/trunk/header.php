87a9bd1e6fe4c849133bc75e9e612792f03288ad
eviara <eviara@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-29 02:14:08 +0000

    Minor changes.

--- eyedb-web/trunk/header.php
+++ eyedb-web/trunk/header.php
@@ -24,23 +24,23 @@
 <td id="NavCol" height="700">
 <div id="Nav">
 <ul>
-<li>&raquo; <a href="index.php">Home</a></li>
-<li>&raquo; <a href="keyfeatures.php">Key features</a></li>
-<li>&raquo; <a href="quicktour.php">Quick tour</a></li>
-<li>&raquo; <a href="history.php">History</a></li>
-<li>&raquo; <a href="platforms.php">Platforms</a></li>
-<li>&raquo; <a href="licensing.php">Licensing</a></li>
+<li>&gt; <a href="index.php">Home</a></li>
+<li>&gt; <a href="keyfeatures.php">Key features</a></li>
+<li>&gt; <a href="quicktour.php">Quick tour</a></li>
+<li>&gt; <a href="history.php">History</a></li>
+<li>&gt; <a href="platforms.php">Platforms</a></li>
+<li>&gt; <a href="licensing.php">Licensing</a></li>
 <!-- TBD -->
-<!-- <li>&raquo; <a href="devteam.php">Development team</a></li> -->
-<li>&raquo; <a href="mailinglists.php">Mailing Lists</a></li>
-<li>&raquo; <a href="http://sourceforge.net/projects/eyedb">Sourceforge</a></li>
-<li>&raquo; <a href="http://wiki.eyedb.org">Wiki</a></li>
-<li>&raquo; <a href="http://blog.eyedb.org">Blog</a></li>
+<!-- <li>&gt; <a href="devteam.php">Development team</a></li> -->
+<li>&gt; <a href="mailinglists.php">Mailing Lists</a></li>
+<li>&gt; <a href="http://sourceforge.net/projects/eyedb">Sourceforge</a></li>
+<li>&gt; <a href="http://wiki.eyedb.org">Wiki</a></li>
+<li>&gt; <a href="http://blog.eyedb.org">Blog</a></li>
 <!-- TBD -->
-<!-- <li>&raquo; <a href="irc.php">IRC</a></li> -->
-<li>&raquo; <a href="http://doc.eyedb.org">Documentation</a></li>
+<!-- <li>&gt; <a href="irc.php">IRC</a></li> -->
+<li>&gt; <a href="http://doc.eyedb.org">Documentation</a></li>
 <!-- TBD -->
-<!-- <li>&raquo; <a href="links.php">Links</a></li> -->
+<!-- <li>&gt; <a href="links.php">Links</a></li> -->
 </ul>
 </div>
 
