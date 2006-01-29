43f1d30b199e1ad4663dfc4e57698f778d7a3419
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-29 15:53:44 +0000

    announce

--- eyedb-web/trunk/index.php
+++ eyedb-web/trunk/index.php
@@ -4,12 +4,12 @@
 
 <p>
 EyeDB is an Object Oriented Database Management System (OODBMS) based
-on the ODMG 3.0 specification.
+on the ODMG 3 specification.
 </p>
 
 <p>
-EyeDB features an advanced object model (inheritance, collections,
-arrays, methods, triggers, constraints, reflexivity), a type
+EyeDB features include an advanced object model (inheritance, collections,
+arrays, methods, triggers, constraints, reflexivity), an object
 definition language based on ODMG ODL, an object query and
 manipulation language based on ODMG OQL and programming interfaces for
 C++ and Java.
