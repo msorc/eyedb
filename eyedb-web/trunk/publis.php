540449dd8adc88e2f37eb42f52e938f003c55b88
eviara <eviara@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-01-29 11:35:15 +0000

    Minor changes.

---
+++ eyedb-web/trunk/publis.php
@@ -0,0 +1,135 @@
+<? $title = 'Publications and conferences'; include( 'header.php'); ?>
+
+<h1><?= $title ?></h1>
+
+<h2>EyeDB Papers</h2>
+<ul>
+<li><a href="http://www.sysra.com/eyedb-paper.pdf">The EyeDB OODBMS</a>.<br>
+E. Viara, E. Barillot &amp; G. Vaysseix;<br>
+<i> International Database Engineering and Applications Symposium 1999, Montreal</i>
+August 1999, IEEE publications, pp 390-402, 1999. 
+</li>
+<br>
+<li>Distributing CORBA Views From an OODBMS.<br>
+E. Viara,  E. Barillot and G. Vaysseix,
+<i>International Database Engineering and Applications Symposium 2002</i>, Edmonton July 2002, IEEE publications, pp 116-129, 2002.
+</li>
+</ul>
+
+<h2>Projects using EyeDB</h2>
+<ul>
+<li>The IDB database system and its use in the human genome project: HUGEMAP.<br>
+
+E. Viara, S. Pook, B. Lacroix, M. Tissot, L. Atlan, A. Cohen-Akenine,
+G. Vaysseix &amp; E. Barillot;
+<br>
+
+<i>Interconnection of Molecular Biology Databases</i>, Stanford, California, 1994.
+
+</li>
+<br>
+
+<li> RHmap, a radiation hybrid database for heavy user access and
+a test case for comparing relational and object oriented technologies.<br>
+
+P. Rodriguez-Tomé, G. Vaysseix, E. Viara &amp;  E. Barillot;<br>
+
+  <i>Interconnection of Molecular Biology Databases</i>, Cambridge, United Kingdom, 1995.
+
+</li><br>
+
+<li> Solutions to the interoperation of biological databases.<br>
+
+ E. Barillot, G. Vaysseix,
+ F. Achard, E. Viara, T. Flores &amp;
+P. Rodriguez-Tomé;<br>
+
+  <i> Human Genome Meeting</i>, Heidelberg, Germany, 1996.
+</li><br>
+
+<li> RHYME: An Implementation of Database Interoperation in Molecular Biology
+    based on distributed and architecture-independant techniques.<br>
+
+ E. Barillot,
+F. Guyon, E. Viara &amp; G. Vaysseix;<br>
+
+  <i> Human Genome Meeting</i>, Toronto, Canada, 1997.
+</li><br>
+
+<li> HuGeMap: a distributed and integrated Human Genome Map database.<br>
+ E. Barillot,
+F. Guyon, C. Cussat-Blanc, E. Viara &amp; G. Vaysseix;<br>
+
+  <i>Nucleic Acids Research</i>, Vol. 26, pp 106-107, 1998.
+</li><br>
+
+<li> Virgil: a database of rich links between GDB and Genbank.<br>
+ F. Achard &amp; 
+ E. Barillot;<br>
+
+  <i>Nucleic Acids Research</i>, Vol. 26, pp 100-101, 1998.
+</li><br>
+
+<li> The new Virgil database: a service of rich links.<br>
+ F. Achard, C. Cussat-Blanc, E. Viara &amp; 
+ E. Barillot;<br>
+
+<i>BIOINFORMATICS</i>, Vol. 14, pp 342-348, 1998.
+ 
+</li><br>
+
+<li>A standard representation for links between genome objects<br>
+F. Achard, C. Cussat-Blanc, E. Viara, G. Vaysseix, P. Dessen and E. Barillot,
+<i>Human Genome Meeting</i>, Turin, Italy, 1998.
+</li><br>
+
+<li><a target="blank" href="http://www.infobiogen.fr/services/zomit/bioinformatics1998">Zomit: biological data visualisation and browsing</a>.<br>
+
+S. Pook, G. Vaysseix &amp;
+ E. Barillot<br>
+
+<i> BIOINFORMATICS</i>, Vol. 14 No. 9, pp 807-814, 1998.
+
+</li><br>
+
+<li> Virgil: a databank of links between GDB and Genbank.<br>
+<i>BIOINFORMATICS</i>, Vol. 14, 342-348, 1998.
+F. Achard &amp; 
+E. Barillot;
+</li><br>
+
+<li>
+    Distributing and visualising biological objects at Infobiogen.<br>
+
+E. Viara, S. Pook, F. Guyon, C. Cussat-Blanc, G. Vaysseix &amp;
+   E. Barillot>;<br>
+
+    <i>Objects In Bioinformatics</i>, Hinxton, 1998.
+</li><br>
+
+<li> The HuGeMap database: Interconnection and Visualisation of Human Genome Maps.<br>
+
+ E. Barillot, S. Pook, F. Guyon, C. Cussat-Blanc, E. Viara &amp; G. Vaysseix;<br>
+
+    <i> Nucleic Acids Research</i>, in press, 1999.
+</li><br>
+
+<li> Virgil database for rich links (1999 update).<br>
+
+    F. Achard, G. Vaysseix, P. Dessen &amp;  E. Barillot;<br>
+
+    <i> Nucleic Acids Research</i>, in press, 1999.
+<br> 
+</li><br>
+
+<li>The Genetpig database: a tool for comparative mapping in pig (Sus scrofa).<br>
+E. Karsenty, E. Barillot, G. Tosser-Klopp, Y. Lahbib-Mansais, F. Hatey,
+S. Cirera, M. Fredholm, B. Chowdhary, C. B. Jørgensen, M. Sawera, K.
+Wimmers, S. Ponsuksili, R. Davoli, L. Fontanesi, S. Braglia, P.
+Zambonelli, D. Bigi, S. Neuenschwander and J. Gellin.
+<i>        Nucleic Acids Research</i> 2003 Jan 1;31(1):138-41.
+</li>
+
+</ul>
+
+<? include( 'footer.php'); ?>
