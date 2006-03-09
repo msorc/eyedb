152d2d951ca16046721d99d30a3c29b3023373b4
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2006-03-09 14:29:49 +0000

    Removed non-posix thread code

--- eyedb/trunk/src/lib/rpcdb.cc
+++ eyedb/trunk/src/lib/rpcdb.cc
@@ -25,44 +25,22 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 #include <eyedblib/rpc.h>
 #include <eyedblib/rpcdb.h>
 #include <eyedblib/rpc_be.h>
 #include <eyedblib/rpcdb_be.h>
 
-/*@@@@/*why is SOLARIS APART ??*/
-
-#if defined(SOLARIS) || defined(ULTRASOL7)
-#include <synch.h>
-#define MUTEX_T mutex_t
-#define MUTEX_LOCK mutex_lock
-#define MUTEX_UNLOCK mutex_unlock
-#endif
-
-#if defined(LINUX) || defined(LINUX64) || defined(LINUX_IA64) || defined(LINUX_PPC64) || defined(ORIGIN) || defined(ALPHA)  || defined(AIX) || defined(CYGWIN)
-#include <pthread.h>
-#define MUTEX_T pthread_mutex_t
-#define MUTEX_LOCK pthread_mutex_lock
-#define MUTEX_UNLOCK pthread_mutex_unlock
-#endif
-
-static MUTEX_T mp_dbhinfo, mp_sem;
+static pthread_mutex_t mp_dbhinfo, mp_sem;
 
 void rpcDB_mutexInit(void)
 {
-  /*@@@@*/
-#if defined(SOLARIS) || defined(ULTRASOL7)
-  mutex_init(&mp_dbhinfo, USYNC_PROCESS, 0);
-  mutex_init(&mp_sem, USYNC_PROCESS, 0);
-#endif
-#if defined(LINUX) || defined(LINUX64) || defined(LINUX_IA64) || defined(LINUX_PPC64) || defined(ORIGIN) || defined(ALPHA) || defined(CYGWIN)
   pthread_mutexattr_t mattr;
 
   pthread_mutexattr_init(&mattr);
   pthread_mutex_init(&mp_dbhinfo, &mattr);
   pthread_mutex_init(&mp_sem, &mattr);
-#endif
 }
 
 int
@@ -91,17 +69,17 @@ rpcDB_dbhinfoNew(int id)
 {
   int i;
 
-  MUTEX_LOCK(&mp_dbhinfo);
+  pthread_mutex_lock(&mp_dbhinfo);
   for (i = 0; i < MAX_DBHINFO; i++)
     if (!rpcDB_dbhInfo[i])
       {
 	rpcDB_dbhInfo[i] = (rpcDB_DbHandleInfo *)calloc(sizeof(rpcDB_DbHandleInfo), 1);
 	rpcDB_dbhInfo[i]->id = id;
 	rpcDB_dbhInfo[i]->refcnt = 1;
-	MUTEX_UNLOCK(&mp_dbhinfo);
+	pthread_mutex_unlock(&mp_dbhinfo);
 	return rpcDB_dbhInfo[i];
       }
-  MUTEX_UNLOCK(&mp_dbhinfo);
+  pthread_mutex_unlock(&mp_dbhinfo);
   return 0;
 }
 
@@ -110,7 +88,7 @@ rpcDB_dbhinfoGet(int id)
 {
   int i;
 
-  MUTEX_LOCK(&mp_dbhinfo);
+  pthread_mutex_lock(&mp_dbhinfo);
   for (i = 0; i < MAX_DBHINFO; i++)
     if (rpcDB_dbhInfo[i] && rpcDB_dbhInfo[i]->id == id)
       {
@@ -118,29 +96,25 @@ rpcDB_dbhinfoGet(int id)
 #ifdef TRACE
 	printf("incrementing dbhinfo %d\n", rpcDB_dbhInfo[i]->refcnt);
 #endif
-	MUTEX_UNLOCK(&mp_dbhinfo);
+	pthread_mutex_unlock(&mp_dbhinfo);
 	return rpcDB_dbhInfo[i];
       }
-  MUTEX_UNLOCK(&mp_dbhinfo);
+  pthread_mutex_unlock(&mp_dbhinfo);
   return 0;
 }
 
 rpcDB_ClientInfo *
 rpcDB_clientInfoGet(rpc_ClientId clientid)
 {
-#if 0
-  static rpcDB_ClientInfo rpcDB_clientsInfo[512];
-  return &rpcDB_clientsInfo[(int)clientid];
-#else
   static rpcDB_ClientInfo rpcDB_clientInfo;
+
   return &rpcDB_clientInfo;
-#endif
 }
 
 void
 rpcDB_clientDbhDelete(rpcDB_DbHandleClientInfo *dbhclientinfo)
 {
-  MUTEX_LOCK(&mp_dbhinfo);
+  pthread_mutex_lock(&mp_dbhinfo);
 
   if (--dbhclientinfo->dbhinfo->refcnt == 0)
     {
@@ -154,7 +128,7 @@ rpcDB_clientDbhDelete(rpcDB_DbHandleClientInfo *dbhclientinfo)
 	  }
     }
   free(dbhclientinfo);
-  MUTEX_UNLOCK(&mp_dbhinfo);
+  pthread_mutex_unlock(&mp_dbhinfo);
 }
 
 int
@@ -195,38 +169,14 @@ rpcDB_clientDbhGet(rpc_ClientId clientid, int id)
 void
 rpcDB_lock()
 {
-  MUTEX_LOCK(&mp_dbhinfo);
+  pthread_mutex_lock(&mp_dbhinfo);
 }
 
 void
 rpcDB_unlock()
 {
-  MUTEX_UNLOCK(&mp_dbhinfo);
+  pthread_mutex_unlock(&mp_dbhinfo);
 }
-
-#if 0
-rpcDB_DbHandleInfo *
-rpcDB_open_realize(rpc_Server *server, int id)
-{
-  rpcDB_DbHandleInfo *dbhinfo;
-
-  dbhinfo = rpcDB_dbhinfoGet(id);
-
-  if (!dbhinfo)
-    {
-      dbhinfo = rpcDB_dbhinfoNew(id);
-#ifndef LOCKMTX
-      MUTEX_LOCK(&mp_sem);
-      dbhinfo->ldbctx.semid[0] =
-	sem_find(&dbhinfo->ldbctx.semkey[0], rpc_True);
-      dbhinfo->ldbctx.semid[1] =
-	sem_find(&dbhinfo->ldbctx.semkey[1], rpc_True);
-      MUTEX_UNLOCK(&mp_sem);
-#endif
-    }
-  return dbhinfo;
-}
-#endif
 
 rpcDB_DbHandleInfo *
 rpcDB_open_simple_realize(rpc_Server *server, int id)
