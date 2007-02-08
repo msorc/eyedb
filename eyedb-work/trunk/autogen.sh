be512cd0a55ade88a930300993efe92b4dabd084
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2007-02-08 21:58:39 +0000

    fixed -I

--- eyedb-work/trunk/autogen.sh
+++ eyedb-work/trunk/autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 [ ! -d config/ ] && mkdir config/
-aclocal -I m4/extras/ -I m4/java/
+aclocal -I m4/java/
 # On Mac OS X, libtoolize is named glibtoolize
 libtoolize --force --automake --copy || glibtoolize --force --automake --copy
 automake  --foreign --add-missing --force-missing --copy
