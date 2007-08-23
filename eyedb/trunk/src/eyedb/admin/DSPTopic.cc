6dc6789304ff06c266ca00eaede3d4b2fdec8990
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2007-08-23 11:37:21 +0000

    * dataspace in progress

--- eyedb/trunk/src/eyedb/admin/DSPTopic.cc
+++ eyedb/trunk/src/eyedb/admin/DSPTopic.cc
@@ -1,5 +1,3 @@
-
-
 /* 
    EyeDB Object Database Management System
    Copyright (C) 1994-1999,2004-2006 SYSRA
@@ -24,19 +22,21 @@
 */
 
 #include "eyedbconfig.h"
-
 #include <eyedb/eyedb.h>
+#include <eyedb/opts.h>
 #include "eyedb/DBM_Database.h"
-#include <sys/types.h>
-#include <signal.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <eyedblib/butils.h>
-
 #include "GetOpt.h"
-
 #include "DSPTopic.h"
+
+using namespace eyedb;
+using namespace std;
+
+#define CHECK_STATUS(s) 			\
+  if (s) {					\
+    std::cerr << PROG_NAME;			\
+    s->print();					\
+    return 1;					\
+  }
 
 DSPTopic::DSPTopic() : Topic("dataspace")
 {
@@ -52,3 +52,430 @@ DSPTopic::DSPTopic() : Topic("dataspace")
   addCommand(new DSPSetCurDatCmd(this));
   addCommand(new DSPGetCurDatCmd(this));
 }
+
+// DSPCreateCmd 
+void DSPCreateCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPCreateCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname> <dspname> {<datid>|<datname>}\n";
+  return 1;
+}
+
+int DSPCreateCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<dbname>", "Data base name");
+  getopt->displayOpt("<dspname>", "Data space name");
+  getopt->displayOpt("<datid>", "Data file id");
+  getopt->displayOpt("<datname>", "Data file name");
+  return 1;
+}
+
+int DSPCreateCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPUpdateCmd 
+void DSPUpdateCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPUpdateCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname> <dspname> {<datid>|<datname>}\n";
+  return 1;
+}
+
+int DSPUpdateCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPUpdateCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPDeleteCmd 
+void DSPDeleteCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPDeleteCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname> <dspname>\n";
+  return 1;
+}
+
+int DSPDeleteCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPDeleteCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPRenameCmd 
+void DSPRenameCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPRenameCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " ?\n";
+  return 1;
+}
+
+int DSPRenameCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPRenameCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPListCmd 
+void DSPListCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPListCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname> [{<dspname>}]\n";
+  return 1;
+}
+
+int DSPListCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPListCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPSetDefCmd 
+void DSPSetDefCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPSetDefCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname> <dspname>\n";
+  return 1;
+}
+
+int DSPSetDefCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPSetDefCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPGetDefCmd 
+void DSPGetDefCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPGetDefCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname>\n";
+  return 1;
+}
+
+int DSPGetDefCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPGetDefCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPSetCurDatCmd 
+void DSPSetCurDatCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPSetCurDatCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname> <dspname> <datid>|<datname>\n";
+  return 1;
+}
+
+int DSPSetCurDatCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPSetCurDatCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+// DSPGetCurDatCmd 
+void DSPGetCurDatCmd::init()
+{
+  std::vector<Option> opts;
+  opts.push_back(HELP_OPT);
+  getopt = new GetOpt(getExtName(), opts);
+}
+
+int DSPGetCurDatCmd::usage()
+{
+  getopt->usage("", "");
+  std::cerr << " <dbname> <dspname>\n";
+  return 1;
+}
+
+int DSPGetCurDatCmd::help()
+{
+  stdhelp();
+  getopt->displayOpt("<user>", "User name");
+  getopt->displayOpt("<passwd>", "Password for specified user");
+  return 1;
+}
+
+int DSPGetCurDatCmd::perform(eyedb::Connection &conn, std::vector<std::string> &argv)
+{
+  if (! getopt->parse(PROG_NAME, argv))
+    return usage();
+
+  GetOpt::Map &map = getopt->getMap();
+
+  if (map.find("help") != map.end())
+    return help();
+
+  std::cerr << " not yet implemented\n";
+  return 1;
+
+  //  DBM_Database *dbmdatabase = new DBM_Database();
+
+  //  conn.open();
+
+  //  Status s = dbmdatabase->addUser(&conn, username, passwd, user_type, userauth, passwdauth);
+
+  //  CHECK_STATUS(s);
+
+  return 0;
+}
+
+
