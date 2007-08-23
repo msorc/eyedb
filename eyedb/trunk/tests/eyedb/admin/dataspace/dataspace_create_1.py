6679aed784b812736e7e15e83a809b6a52df3d37
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2007-08-23 14:11:21 +0000

    * dataspace create tested

--- eyedb/trunk/tests/eyedb/admin/dataspace/dataspace_create_1.py
+++ eyedb/trunk/tests/eyedb/admin/dataspace/dataspace_create_1.py
@@ -4,6 +4,12 @@ import sys
 dbname='foo'
 dspname='bar'
 datname='bar.dat'
+
+# create the data file
+child = pexpect.spawn("eyedbadmin datcreate %s %s" % (dbname,datname))
+r = child.expect(pexpect.EOF)
+child.close()
+# create the dataspace
 command="eyedbadmin2 dataspace create %s %s %s" % (dbname,dspname,datname)
 child = pexpect.spawn(command)
 r = child.expect(pexpect.EOF)
