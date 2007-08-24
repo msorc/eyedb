e4b6d9ec3ee31fc54b43acb3a1c7a8c2e2d780a2
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2007-08-24 16:20:50 +0000

    * started index

--- eyedb/trunk/tests/eyedb/admin/index/index_create_1.py
+++ eyedb/trunk/tests/eyedb/admin/index/index_create_1.py
@@ -5,7 +5,7 @@ dbname='foo'
 dspname='bar'
 datname='bar.dat'
 
-# create the dataspace
+# create the index
 command="eyedbadmin2 index create %s %s %s" % (dbname,dspname,datname)
 child = pexpect.spawn(command)
 r = child.expect(pexpect.EOF)
