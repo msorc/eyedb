f722fc9c3f6a6284abe671c8d148469925143e62
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2007-10-11 14:11:58 +0000

    * theme in progress

---
+++ eyedb-web/trunk/wordpress/themes/eyedb/src/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
+<meta name="keywords" content="eyedb, database, object" />
+
+<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+
+<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('stylesheet_url'); ?>" />
+
+</head>
+
+<body>
+
+<h1>EyeDB</h1>
