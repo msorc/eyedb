5067fcb0947ef3c52e472994a52c36abf7081c1d
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-05-13 20:09:22 +0000

    speedup removing remote images

--- eyedb-web/trunk/wordpress/themes/eyedb/footer.php
+++ eyedb-web/trunk/wordpress/themes/eyedb/footer.php
@@ -1,8 +1,7 @@
 <div id="footer">
 <?php wp_footer(); // Plugin API footer hook ?>
-<a class="sourceforge" href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=127988&type=1" width="88" height="31" alt="SourceForge.net Logo" /></a>
 <span id="cclicense">
-<a rel="license" href="http://creativecommons.org/licenses/by-sa/2.0/fr/"><img alt="Creative Commons License" src="http://i.creativecommons.org/l/by-sa/2.0/fr/88x31.png"/></a><div>Content on this site is licensed under<br/>a <a rel="license" href="http://creativecommons.org/licenses/by-sa/2.0/fr/">Creative Commons License</a></div></span>
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/2.0/fr/"><img alt="Creative Commons License" src="/wp-content/themes/eyedb/images/creativecommons-by-sa-88x31.png"/></a><div>Content on this site is licensed under<br/>a <a rel="license" href="http://creativecommons.org/licenses/by-sa/2.0/fr/">Creative Commons License</a></div></span>
 <span id="meta">
 <a href="/wp-login.php">Login</a>&nbsp;|&nbsp;<a href="mailto:info@eyedb.org">Contact</a>
 </span>
