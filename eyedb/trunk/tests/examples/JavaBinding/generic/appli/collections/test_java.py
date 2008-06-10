61f78c42c63e205bd3028f132391d8ccbd2978a6
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-06-10 13:52:59 +0000

    all Java examples implemented as tests and passed

--- eyedb/trunk/tests/examples/JavaBinding/generic/appli/collections/test_java.py
+++ eyedb/trunk/tests/examples/JavaBinding/generic/appli/collections/test_java.py
@@ -2,4 +2,4 @@ import os
 from eyedb.test.command import run_java_command
 
 subdir = "%s/examples/JavaBinding/generic/appli/collections" % (os.environ['top_builddir'])
-run_java_command( 'Collections', args='person_j toto', classpath=[subdir])
+run_java_command( 'Collections', args=['person_j', 'toto'], classpath=[subdir])
--- eyedb/trunk/tests/examples/JavaBinding/generic/appli/query/test_java.py
+++ eyedb/trunk/tests/examples/JavaBinding/generic/appli/query/test_java.py
@@ -2,4 +2,4 @@ import os
 from eyedb.test.command import run_java_command
 
 subdir = "%s/examples/JavaBinding/generic/appli/query" % (os.environ['top_builddir'])
-run_java_command( 'Query', args="person_j 'select Person'", classpath=[subdir])
+run_java_command( 'Query', args=['person_j','select Person'], classpath=[subdir])
