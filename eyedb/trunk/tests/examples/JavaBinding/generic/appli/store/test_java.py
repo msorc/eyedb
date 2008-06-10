9eae4a09232b1143ff646fffcd5d613ffba402a8
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-06-10 13:52:59 +0000

    all Java examples implemented as tests and passed

--- eyedb/trunk/tests/examples/JavaBinding/generic/appli/store/test_java.py
+++ eyedb/trunk/tests/examples/JavaBinding/generic/appli/store/test_java.py
@@ -2,4 +2,4 @@ import os
 from eyedb.test.command import run_java_command
 
 subdir = "%s/examples/JavaBinding/generic/appli/store" % (os.environ['top_builddir'])
-run_java_command( 'Store', args='person_j toto 42', classpath=[subdir])
+run_java_command( 'Store', args=['person_j', 'toto', '42'], classpath=[subdir])
