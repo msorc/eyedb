ca2bc8ea9f141f898ead032f9a5c8c305bb10be6
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-09-25 19:58:38 +0000

    Barcelona :( benchmark compiling OK

--- eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/eyedb/BarcelonaEyeDB.java
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/eyedb/BarcelonaEyeDB.java
@@ -1,11 +1,49 @@
 package org.eyedb.benchmark.polepos.teams.eyedb;
 
+import org.eyedb.benchmark.polepos.teams.eyedb.barcelona.Database;
+import org.eyedb.benchmark.polepos.teams.eyedb.barcelona.B4;
 import org.polepos.circuits.barcelona.BarcelonaDriver;
+import org.polepos.framework.CarMotorFailureException;
 
 public class BarcelonaEyeDB extends EyeDBDriver implements BarcelonaDriver {
     
+    public void prepare() throws CarMotorFailureException 
+    {
+	try {
+	    org.eyedb.benchmark.polepos.teams.eyedb.barcelona.Database.init();
+	    
+	    getEyeDBCar().openConnection( new org.eyedb.benchmark.polepos.teams.eyedb.barcelona.Database( "barcelona"));
+	}
+	catch( org.eyedb.Exception e) {
+	    e.printStackTrace();
+            throw new CarMotorFailureException();
+	}
+    }
+
     public void write()
     {
+        try {
+	    getDatabase().transactionBegin();
+            
+            int count = setup().getObjectCount(); 
+
+            for ( int i = 0; i < count; i++) {
+                B4 b4 = new B4( getDatabase());
+
+                b4.setB0( i+1);
+                b4.setB1( i+1);
+                b4.setB2( i+1);
+                b4.setB3( i+1);
+                b4.setB4( i+1);
+
+		b4.store( org.eyedb.RecMode.FullRecurs);
+            }
+            
+            getDatabase().transactionCommit();
+        }
+        catch ( org.eyedb.Exception ex ) {
+            ex.printStackTrace();
+        }
     }
     
     public void read()
--- eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/eyedb/EyeDBDriver.java
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/eyedb/EyeDBDriver.java
@@ -1,19 +1,27 @@
 package org.eyedb.benchmark.polepos.teams.eyedb;
 
-import java.util.*;
-
-import org.polepos.framework.Car;
+import org.eyedb.Database;
 import org.polepos.framework.Driver;
-import org.polepos.framework.TurnSetup;
-import org.polepos.framework.CarMotorFailureException;
 
 public abstract class EyeDBDriver extends Driver {
 
-    public void prepare() throws CarMotorFailureException 
-    {
-    }
-
     public void backToPit()
     {
+	try {
+	    getEyeDBCar().closeConnection();
+	}
+	catch( org.eyedb.Exception e) {
+	    e.printStackTrace();
+	}
+    }
+
+    EyeDBCar getEyeDBCar()
+    {
+	return (EyeDBCar)car();
+    }
+
+    Database getDatabase()
+    {
+	return getEyeDBCar().getDatabase();
     }
 }
