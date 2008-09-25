f979acc95a94e2897e02d0258c5c03a5a3618163
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-09-25 22:26:18 +0000

    Barcelona :( benchmark implementation completed

--- eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/eyedb/EyeDBDriver.java
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/eyedb/EyeDBDriver.java
@@ -1,6 +1,9 @@
 package org.eyedb.benchmark.polepos.teams.eyedb;
 
+import java.util.Iterator;
 import org.eyedb.Database;
+import org.eyedb.ObjectArray;
+import org.eyedb.OQL;
 import org.polepos.framework.Driver;
 
 public abstract class EyeDBDriver extends Driver {
@@ -23,5 +26,42 @@ public abstract class EyeDBDriver extends Driver {
     Database getDatabase()
     {
 	return getEyeDBCar().getDatabase();
+    }
+
+    class QueryIterator implements Iterator {
+	QueryIterator( OQL query) throws org.eyedb.Exception
+	{
+	    ObjectArray result = new ObjectArray();
+
+	    query.execute( result);
+
+	    i = 0;
+	    count = result.getCount();
+	    objects = result.getObjects();
+	}
+
+	public boolean hasNext()
+	{
+	    return i < count;
+	}
+
+	public Object next()
+	{
+	    return objects[i++];
+	}
+
+	public void remove()
+	{
+	    throw new UnsupportedOperationException();
+	}
+
+	private int i;
+	private int count;
+	private Object[] objects;
+    }
+
+    Iterator iterate( String q) throws org.eyedb.Exception
+    {
+	return new QueryIterator( new OQL( getDatabase(), q));
     }
 }
