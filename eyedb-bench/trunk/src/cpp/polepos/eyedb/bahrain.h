93d9987ef5d6401e6f7e55d911ba20d62a2ca7e5
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-11-01 12:11:10 +0000

    adding C++ implementations for remaining poleposition benchmarks

---
+++ eyedb-bench/trunk/src/cpp/polepos/eyedb/bahrain.h
@@ -0,0 +1,24 @@
+#ifndef _BAHRAIN_H_
+#define _BAHRAIN_H_
+
+#include "polepos-benchmark.h"
+
+class Bahrain : public PoleposBenchmark {
+
+public:
+
+  void write( int count);
+  void query_indexed_string( int selectCount);
+  void query_string( int selectCount);
+  void query_indexed_int( int selectCount);
+  void query_int( int selectCount);
+  void update( int updateCount);
+  void destroy();
+
+  void run();
+
+  const char* getName() { return "Bahrain"; }
+  const char* getDescription() { return "write, query, update and delete simple flat objects individually"; }
+};
+
+#endif
--- eyedb-bench/trunk/src/cpp/polepos/eyedb/barcelona.h
+++ eyedb-bench/trunk/src/cpp/polepos/eyedb/barcelona.h
@@ -1,34 +1,21 @@
 #ifndef _BARCELONA_H_
 #define _BARCELONA_H_
 
-#include <eyedb/eyedb.h>
-#include "benchmark.h"
+#include "polepos-benchmark.h"
 
-class Barcelona : public eyedb::benchmark::Benchmark {
+class Barcelona : public PoleposBenchmark {
 
 public:
-  Barcelona()
-  {
-  }
 
   void write( int count);
   void read();
   void query( int selectCount);
   void destroy();
 
-  void prepare();
   void run();
-  void finish();
 
   const char* getName() { return "Barcelona"; }
   const char* getDescription() { return "writes, reads, queries and deletes objects with a 5 level inheritance structure"; }
-  const char* getRunDescription();
-
-  eyedb::Database *getDatabase() { return database; }
-
-private:
-  eyedb::Database *database;
-  eyedb::Connection *conn;
 };
 
 #endif
