a480b6d75c3fc755cb9ce3d5eb14822440d0fa0a
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-11-02 14:37:40 +0000

    poleposition C++ implementation in progress

--- eyedb-bench/trunk/src/cpp/framework/benchmark.h
+++ eyedb-bench/trunk/src/cpp/framework/benchmark.h
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <map>
+#ifndef _EYEDB_BENCHMARK_BENCHMARK_
+#define _EYEDB_BENCHMARK_BENCHMARK_
+
 #include "stopwatch.h"
+#include "properties.h"
 
 namespace eyedb {
   namespace benchmark {
     class Benchmark {
     public:
 
-      Benchmark();
-
-      virtual const char* getName() = 0;
-      virtual const char* getDescription() = 0;
-      virtual const char* getRunDescription() = 0;
+      virtual const char* getName() const = 0;
+      virtual const char* getDescription() const = 0;
+      virtual const char* getRunDescription() const = 0;
       
       virtual void prepare() = 0;
       virtual void run() = 0;
@@ -19,42 +19,15 @@ namespace eyedb {
 
       void bench();
 
-      void setColumnWidth( int width) { columnWidth = width; }
-      void setColumnSeparator( char separator) { columnSeparator = separator; }
-      void addColumnHeader( const std::string &header) { columnHeaders.push_back( header); }
-      void addRowHeader( const std::string &header) { rowHeaders.push_back( header); }
-      void reportBegin();
-      void reportLaps();
-      void reportEnd();
+      const StopWatch &getStopwatch() const { return stopwatch; }
 
-      StopWatch &getStopwatch() { return stopwatch; }
-
-      void loadProperties( const std::string &filename);
-      void loadProperties( std::istream &is);
-      void loadProperties( int &argc, char **argv);
-
-      int getIntProperty( const std::string &name, int &value, int defaultValue = -1);
-      int getIntProperty( const std::string &name, std::vector<int> &values);
-
-      int getStringProperty( const std::string &name, std::string &value, const std::string &defaultValue = "");
-
-      void printProperties();
+      Properties &getProperties() { return properties; }
 
     private:
-      void lexicalError( char c, int pos, int line);
-
       StopWatch stopwatch;
-      std::map<const std::string, std::string> properties;
-
-      std::vector< std::string> columnHeaders;
-      std::vector< std::string> rowHeaders;
-      int columnWidth;
-      char columnSeparator;
-      bool reportLapsDone;
-      bool reportColumnHeadersDone;
-
-      static const int defaultColumnWidth;
-      static const char defaultColumnSeparator;
+      Properties properties;
     };
   };
 };
+
+#endif
