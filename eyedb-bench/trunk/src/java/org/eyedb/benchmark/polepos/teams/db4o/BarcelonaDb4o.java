23faf44ab275abf1acd2ad014493466a443730d5
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-11-03 15:50:20 +0000

    adding modified db4o Barcelona implementation

---
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/db4o/BarcelonaDb4o.java
@@ -0,0 +1,20 @@
+package org.eyedb.benchmark.polepos.teams.db4o;
+
+public class BarcelonaDb4o extends org.polepos.teams.db4o.BarcelonaDb4o {
+
+    public void write()
+    {
+    }
+
+    public void read()
+    {
+    }
+
+    public void query()
+    {
+    }
+
+    public void delete()
+    {
+    }
+}
