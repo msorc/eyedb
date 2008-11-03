ee21e1d2e923a20e084fb08db0e74a1d1b05b491
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-11-03 22:15:27 +0000

    tried splitting db4o benches in 2 phases, write and read/query/delete; added client/server version of db4o

--- eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/db4o/BarcelonaDb4o.java
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/db4o/BarcelonaDb4o.java
@@ -1,20 +1,56 @@
 package org.eyedb.benchmark.polepos.teams.db4o;
 
+import org.polepos.framework.CheckSummable;
+import org.polepos.circuits.barcelona.B4;
+import com.db4o.ObjectSet;
+import com.db4o.query.Query;
+
 public class BarcelonaDb4o extends org.polepos.teams.db4o.BarcelonaDb4o {
 
     public void write()
     {
+	String p = System.getProperties().getProperty("barcelona.write");
+
+	if (p != null && p.equals("true"))
+	    super.write();
     }
 
     public void read()
     {
+	String p = System.getProperties().getProperty("barcelona.read");
+
+	if (p != null && p.equals("true")) {
+	    Query q = db().query();
+	    q.constrain( B4.class);
+
+	    ObjectSet result = doQuery( q);
+
+// 	    ObjectSet result = q.execute();
+
+// 	    while (result.hasNext()) {
+// 		Object o = result.next();
+// 		if (o instanceof CheckSummable) {
+// 		    addToCheckSum(((CheckSummable) o).checkSum());
+// 		}
+// 	    }
+
+	    System.err.println( "Read: " + result.size() + " objects of class " + B4.class.getName());
+	}
     }
 
     public void query()
     {
+	String p = System.getProperties().getProperty("barcelona.query");
+
+	if (p != null && p.equals("true"))
+	    super.query();
     }
 
     public void delete()
     {
+	String p = System.getProperties().getProperty("barcelona.delete");
+
+	if (p != null && p.equals("true"))
+	    super.delete();
     }
 }
--- eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/db4o/Db4oTeam.java
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/polepos/teams/db4o/Db4oTeam.java
@@ -1,8 +1,22 @@
 package org.eyedb.benchmark.polepos.teams.db4o;
 
 import org.polepos.framework.Driver;
+import org.polepos.teams.db4o.Db4oOptions;
 
 public class Db4oTeam extends org.polepos.teams.db4o.Db4oTeam {
+
+
+    public Db4oTeam()
+    {
+	super();
+
+	int[] options = new int[] { 
+	    Db4oOptions.CLIENT_SERVER,
+	    Db4oOptions.CLIENT_SERVER_TCP 
+	};
+
+	configure( options);
+    }
 
     public String name()
     {
