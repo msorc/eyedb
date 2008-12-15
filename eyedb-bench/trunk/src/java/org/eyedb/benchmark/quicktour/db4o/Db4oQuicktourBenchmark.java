d0f96702810148f9532bbadb5f36ada1be2fdbd4
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2008-12-15 16:56:59 +0000

    db4o quicktour almost complete

--- eyedb-bench/trunk/src/java/org/eyedb/benchmark/quicktour/db4o/Db4oQuicktourBenchmark.java
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/quicktour/db4o/Db4oQuicktourBenchmark.java
@@ -1,9 +1,11 @@
 package org.eyedb.benchmark.quicktour.db4o;
 
 import org.eyedb.benchmark.quicktour.QuicktourBenchmark;
-
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Predicate;
+import com.db4o.query.Query;
 
 /**
  * @author Fran&ccedil;ois D&eacute;chelle (francois@dechelle.net)
@@ -11,99 +13,164 @@ import com.db4o.ObjectContainer;
 
 public class Db4oQuicktourBenchmark extends QuicktourBenchmark {
 
-	public String getImplementation()
-	{
-		return "db4o implementation";
+    public String getImplementation()
+    {
+	return "db4o implementation";
+    }
+
+    public void prepareClient()
+    {
+	String host = getProperties().getProperty("db4o.host");
+	int port = getProperties().getIntProperty("db4o.port");
+	String user = getProperties().getProperty("db4o.user");
+	String passwd = getProperties().getProperty("db4o.passwd");
+
+	try {
+	    container = Db4o.openClient( host, port, user, passwd);
+	}
+	catch( Exception e) {
+	    e.printStackTrace();
+	    System.exit( 1);
+	}
+    }
+
+    public void prepare()
+    {
+	String filename = getProperties().getProperty( "db4o.file");
+
+	container = Db4o.openFile( filename);		
+
+        Db4o.configure().objectClass( Student.class ).objectField( "lastName" ).indexed( true );
+    }
+
+    public void finish()
+    {
+	container.close();
+    }
+
+    private Teacher[] fillTeachers( int nTeachers)
+    {
+	Teacher[] teachers = new Teacher[nTeachers];
+
+	for ( int n = 0; n < nTeachers; n++) {
+	    Teacher t = new Teacher();
+	    t.setFirstName( "Teacher_"+n+"_firstName");
+	    t.setLastName( "Teacher_"+n);
+	    teachers[n] = t;
+
+	    container.store( t);
 	}
 
-	public void prepareClient()
-	{
-		String host = getProperties().getProperty("db4o.host");
-		int port = getProperties().getIntProperty("db4o.port");
-		String user = getProperties().getProperty("db4o.user");
-		String passwd = getProperties().getProperty("db4o.passwd");
+	container.commit();
 
-		try {
-			container = Db4o.openClient( host, port, user, passwd);
-		}
-		catch( Exception e) {
-			e.printStackTrace();
-			System.exit( 1);
-		}
+	return teachers;
+    }
+
+    private Course[] fillCourses( int nCourses, Teacher[] teachers)
+    {
+	Course courses[] = new Course[ nCourses];
+
+	for ( int n = 0; n < nCourses; n++) {
+	    Course c = new Course();
+	    c.setTitle( "Course_"+n);
+	    c.setDescription( "Description of course "+n);
+	    c.setTeacher( teachers[ getRandom().nextInt( teachers.length)]);
+	    courses[n] = c;
+
+	    container.store( c);
 	}
 
-	public void prepare()
-	{
-		String filename = getProperties().getProperty( "db4o.filename");
+	container.commit();
 
-		container = Db4o.openFile( filename);		
+	return courses;
+    }
+
+    public void create(int nStudents, int nCourses, int nTeachers, int nObjectsPerTransaction)
+    {
+	Teacher[] teachers = fillTeachers( nTeachers);
+	Course[] courses = fillCourses( nCourses, teachers);
+
+	for (int n = 0; n < nStudents; n++ ) {
+	    Student student = new Student();
+	    student.setFirstName("Student_"+n+"_firstName");
+	    student.setLastName("Student_"+n);
+	    //		    student.setBeginYear( (short)(random.nextInt( 3) + 1));
+
+	    for ( int c = 0; c < courses.length; c++) {
+		int i = getRandom().nextInt( courses.length);
+		student.getCourses().add( courses[i]);
+	    }
+
+	    container.store( student);
+
+	    if (n % nObjectsPerTransaction == nObjectsPerTransaction - 1) {
+		container.commit();
+	    }
 	}
 
-	public void finish()
+	container.commit();
+
+    }
+
+    private static class StudentFind extends Predicate<Student> {
+	StudentFind( String pattern)
+	    {
+		this.pattern = pattern;
+	    }
+
+	public boolean match( Student p)
 	{
-		container.close();
+	    return p.getLastName().matches( pattern);
 	}
 
-	private Teacher[] fillTeachers( int nTeachers)
-	{
-		Teacher[] teachers = new Teacher[nTeachers];
+	private String pattern;
+    }
 
-		for ( int n = 0; n < nTeachers; n++) {
-			Teacher t = new Teacher();
-			t.setFirstName( "Teacher_"+n+"_firstName");
-			t.setLastName( "Teacher_"+n);
-			teachers[n] = t;
-		}
-
-		return teachers;
+    private void queryStudentsNative( int nSelects)
+    {
+	for ( int n = 0;  n < nSelects; n++) {
+	    StudentFind f = new StudentFind( ""+n);
+	    for (Student student : container.query( f)) {
+		System.out.println( student);
+	    }
 	}
 
-	private Course[] fillCourses( int nCourses, Teacher[] teachers)
-	{
-		Course courses[] = new Course[ nCourses];
+	container.commit();
+    }
 
-		for ( int n = 0; n < nCourses; n++) {
-			Course c = new Course();
-			c.setTitle( "Course_"+n);
-			c.setDescription( "Description of course "+n);
-			courses[n].setTeacher( teachers[ getRandom().nextInt( teachers.length)]);
-			courses[n] = c;
-		}
+    private void queryStudentsSODA( int nSelects)
+    {
+	int count = 0;
 
-		return courses;
+	for ( int n = 0;  n < nSelects; n++) {
+            Query q = container.query();
+            q.constrain( Student.class );
+            q.descend( "lastName" ).constrain("Student_" + n);
+
+	    ObjectSet result = q.execute();
+	    count = 0;
+	    while (result.hasNext()) {
+		Student s = (Student)result.next();
+		count++;
+	    }
 	}
 
+	System.out.println( "Retrieved " + count + " students");
 
-	public void create(int nStudents, int nCourses, int nTeachers, int nObjectsPerTransaction)
-	{
-		Teacher[] teachers = fillTeachers( nTeachers);
-		Course[] courses = fillCourses( nCourses, teachers);
+	container.commit();
+    }
 
-		for (int n = 0; n < nStudents; n++ ) {
-			Student student = new Student();
-			student.setFirstName("Student_"+n+"_firstName");
-			student.setLastName("Student_"+n);
-			//		    student.setBeginYear( (short)(random.nextInt( 3) + 1));
 
-			for ( int c = 0; c < courses.length; c++) {
-				int i = getRandom().nextInt( courses.length);
-				student.getCourses().add( courses[i]);
-			}
+    public void query(int nSelects)
+    {
+	//	queryStudentsNative( nSelects);
+	queryStudentsSODA( nSelects);
+    }
 
-			container.store( student);
+    public void remove()
+    {
+    }
 
-		}
-
-	}
-
-	public void query(int nSelects)
-	{
-	}
-
-	public void remove()
-	{
-	}
-
-	private ObjectContainer container;
+    private ObjectContainer container;
 }
 
--- eyedb-bench/trunk/src/java/org/eyedb/benchmark/quicktour/hibernate/HibernateQuicktourBenchmark.java
+++ eyedb-bench/trunk/src/java/org/eyedb/benchmark/quicktour/hibernate/HibernateQuicktourBenchmark.java
@@ -17,201 +17,197 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateQuicktourBenchmark extends QuicktourBenchmark {
 
-	public String getImplementation()
-	{
-		return "Hibernate Java implementation";
+    public String getImplementation()
+    {
+	return "Hibernate Java implementation";
+    }
+
+    private void hack( Configuration cfg)
+    {
+	cfg.setProperty("hibernate.query.substitutions", "true 1, false 0, yes 'Y', no 'N'");
+	cfg.setProperty("hibernate.connection.pool_size", "1");
+	cfg.setProperty("hibernate.proxool.pool_alias", "pool1");
+	cfg.setProperty("hibernate.jdbc.batch_size", "0");
+	cfg.setProperty("hibernate.jdbc.batch_versioned_data", "true");
+	cfg.setProperty("hibernate.jdbc.use_streams_for_binary", "true");
+	cfg.setProperty("hibernate.max_fetch_depth", "1");
+	//		cfg.setProperty("hibernate.cache.region_prefix", "hibernate.test");
+	cfg.setProperty("hibernate.cache.use_query_cache", "true");
+	//		cfg.setProperty("hibernate.cache.provider_class", "net.sf.hibernate.cache.EhCacheProvider");
+
+	cfg.setProperty("hibernate.proxool.pool_alias", "pool1");
+	cfg.setProperty("hibernate.proxool.pool_alias", "pool1");
+    }
+
+    public void prepare()
+    {
+	try {
+	    Configuration configuration = new Configuration();
+	    configuration.configure( new File("hibernate.cfg.xml"));
+
+	    String url = getProperties().getProperty( "hibernate.connection.url");
+	    configuration.setProperty( "connection.url", url);
+	    String username = getProperties().getProperty( "hibernate.connection.username");
+	    configuration.setProperty( "connection.username", username);
+	    String password = getProperties().getProperty( "hibernate.connection.password");
+	    configuration.setProperty( "connection.password", password);
+
+	    //hack( configuration);
+
+	    sessionFactory = configuration.buildSessionFactory();
+
+	    session = sessionFactory.openSession();
+
+	} catch (Throwable ex) {
+	    System.err.println("Initial SessionFactory creation failed." + ex);
+	    ex.printStackTrace();
+	    System.exit(1);
+	}
+    }
+
+    public void finish()
+    {
+	session.close();
+	sessionFactory.close();
+    }
+
+    protected Session getSession()
+    {
+	return session;
+    }
+
+    private Teacher[] fillTeachers( int nTeachers)
+    {
+	Teacher[] teachers = new Teacher[nTeachers];
+
+	getSession().beginTransaction();
+
+	for ( int n = 0; n < nTeachers; n++) {
+	    Teacher t = new Teacher();
+	    t.setFirstName("Teacher_"+n+"_firstName");
+	    t.setLastName("Teacher_"+n);
+	    teachers[n] = t;
+
+	    getSession().save( t);
 	}
 
-	private void hack( Configuration cfg)
-	{
-		cfg.setProperty("hibernate.query.substitutions", "true 1, false 0, yes 'Y', no 'N'");
-		cfg.setProperty("hibernate.connection.pool_size", "1");
-		cfg.setProperty("hibernate.proxool.pool_alias", "pool1");
-		cfg.setProperty("hibernate.jdbc.batch_size", "0");
-		cfg.setProperty("hibernate.jdbc.batch_versioned_data", "true");
-		cfg.setProperty("hibernate.jdbc.use_streams_for_binary", "true");
-		cfg.setProperty("hibernate.max_fetch_depth", "1");
-//		cfg.setProperty("hibernate.cache.region_prefix", "hibernate.test");
-		cfg.setProperty("hibernate.cache.use_query_cache", "true");
-//		cfg.setProperty("hibernate.cache.provider_class", "net.sf.hibernate.cache.EhCacheProvider");
+	getSession().getTransaction().commit();
 
-		cfg.setProperty("hibernate.proxool.pool_alias", "pool1");
-		cfg.setProperty("hibernate.proxool.pool_alias", "pool1");
+	return teachers;
+    }
+
+    private Course[] fillCourses( int nCourses, Teacher[] teachers)
+    {
+	Course courses[] = new Course[ nCourses];
+
+	getSession().beginTransaction();
+
+	for ( int n = 0; n < nCourses; n++) {
+	    Course c = new Course();
+	    c.setTitle("Course_"+n);
+	    c.setDescription("Description of course "+n);
+	    c.setTeacher( teachers[ getRandom().nextInt( teachers.length)]);
+	    courses[n] = c;
+
+	    getSession().save( c);
 	}
 
-	public void prepare()
-	{
-		try {
-			Configuration configuration = new Configuration();
-			configuration.configure( new File("hibernate.cfg.xml"));
+	getSession().getTransaction().commit();
 
-			String url = getProperties().getProperty( "hibernate.connection.url");
-			configuration.setProperty( "connection.url", url);
-			String username = getProperties().getProperty( "hibernate.connection.username");
-			configuration.setProperty( "connection.username", username);
-			String password = getProperties().getProperty( "hibernate.connection.password");
-			configuration.setProperty( "connection.password", password);
+	return courses;
+    }
 
-			//hack( configuration);
+    public void create(int nStudents, int nCourses, int nTeachers, int nObjectsPerTransaction)
+    {
+	Teacher[] teachers = fillTeachers( nTeachers);
+	Course[] courses = fillCourses( nCourses, teachers);
 
-			sessionFactory = configuration.buildSessionFactory();
+	getSession().beginTransaction();
 
-			session = sessionFactory.openSession();
+	for (int n = 0; n < nStudents; n++ ) {
+	    Student student = new Student();
+	    student.setFirstName("Student_"+n+"_firstName");
+	    student.setLastName("Student_"+n);
+	    //		    student.setBeginYear( (short)(random.nextInt( 3) + 1));
 
-		} catch (Throwable ex) {
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			ex.printStackTrace();
-			System.exit(1);
-		}
-	}
+	    for ( int c = 0; c < courses.length; c++) {
+		int i = getRandom().nextInt( courses.length);
+		student.getCourses().add( courses[i]);
+	    }
 
-	public void finish()
-	{
-		session.close();
-		sessionFactory.close();
-	}
+	    getSession().save(student);
 
-	protected Session getSession()
-	{
-		return session;
-	}
-
-	private Teacher[] fillTeachers( int nTeachers)
-	{
-		Teacher[] teachers = new Teacher[nTeachers];
-
-		getSession().beginTransaction();
-
-		for ( int n = 0; n < nTeachers; n++) {
-			Teacher t = new Teacher();
-			t.setFirstName("Teacher_"+n+"_firstName");
-			t.setLastName("Teacher_"+n);
-			teachers[n] = t;
-
-			getSession().save( t);
-		}
-
+	    if (n % nObjectsPerTransaction == nObjectsPerTransaction - 1) {
 		getSession().getTransaction().commit();
-
-		return teachers;
-	}
-
-	private Course[] fillCourses( int nCourses, Teacher[] teachers)
-	{
-		Course courses[] = new Course[ nCourses];
-
 		getSession().beginTransaction();
-
-		for ( int n = 0; n < nCourses; n++) {
-			Course c = new Course();
-			c.setTitle("Course_"+n);
-			c.setDescription("Description of course "+n);
-			c.setTeacher( teachers[ getRandom().nextInt( teachers.length)]);
-			courses[n] = c;
-
-			getSession().save( c);
-		}
-
-		getSession().getTransaction().commit();
-
-		return courses;
+	    }
 	}
 
-	public void create(int nStudents, int nCourses, int nTeachers, int nObjectsPerTransaction)
-	{
-		Teacher[] teachers = fillTeachers( nTeachers);
-		Course[] courses = fillCourses( nCourses, teachers);
+	getSession().getTransaction().commit();
 
-		getSession().beginTransaction();
+	getSession().flush();
+    }
 
-		for (int n = 0; n < nStudents; n++ ) {
-			Student student = new Student();
-			student.setFirstName("Student_"+n+"_firstName");
-			student.setLastName("Student_"+n);
-			//		    student.setBeginYear( (short)(random.nextInt( 3) + 1));
+    private void queryByClass( String className, int nSelects)
+    {
+	try {
+	    getSession().beginTransaction();
 
-			for ( int c = 0; c < courses.length; c++) {
-				int i = getRandom().nextInt( courses.length);
-				student.getCourses().add( courses[i]);
-			}
+	    for ( int n = 0;  n < nSelects; n++) {
+		String s = "from " + className + " as x where x.lastName like ?";
+		Query q = getSession().createQuery( s);
+		q.setString( 0, ""+n);
 
-			getSession().save(student);
-
-			if (n % nObjectsPerTransaction == nObjectsPerTransaction - 1) {
-				getSession().getTransaction().commit();
-				getSession().beginTransaction();
-			}
+		for (Object o: q.list()) {
+		    System.out.println( o);
 		}
+		//				int count = 0;
+		//				Iterator it = q.iterate();
+		//				while(it.hasNext()) 
+		//				count++;
+	    }
 
-		getSession().getTransaction().commit();
-
-		getSession().flush();
+	    getSession().getTransaction().commit();
 	}
-
-	private void queryByClass( String className, int nSelects)
-	{
-		try {
-			Transaction tx = getSession().beginTransaction();
-
-			for ( int n = 0;  n < nSelects; n++) {
-				String s = "from " + className + " as x where x.lastName like ?";
-				Query q = getSession().createQuery( s);
-				q.setString( 0, ""+n);
-
-				for (Object o: q.list()) {
-					Teacher t = (Teacher)o;
-
-					System.out.println( t);
-					for ( Course c: t.getCourses())
-						System.out.println( c);
-				}
-//				int count = 0;
-//				Iterator it = q.iterate();
-//				while(it.hasNext()) 
-//				count++;
-			}
-
-			tx.commit();
-		}
-		catch( HibernateException e) {
-			e.printStackTrace();
-		}
+	catch( HibernateException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	public void query( int nSelects)
-	{
-		queryByClass( "Teacher", nSelects);
-		queryByClass( "Student", nSelects);
+    public void query( int nSelects)
+    {
+	queryByClass( "Teacher", nSelects);
+	queryByClass( "Student", nSelects);
+    }
+
+    private void removeByClass( String className)
+    {
+	try {
+	    Transaction tx = getSession().beginTransaction();
+
+	    String s = "from " + className;
+	    Query q = getSession().createQuery( s);
+
+	    Iterator it = q.iterate();
+	    while(it.hasNext()) 
+		getSession().delete( it.next());
+
+	    tx.commit();
 	}
-
-	private void removeByClass( String className)
-	{
-		try {
-			Transaction tx = getSession().beginTransaction();
-
-			String s = "from " + className;
-			Query q = getSession().createQuery( s);
-
-			Iterator it = q.iterate();
-			while(it.hasNext()) 
-				getSession().delete( it.next());
-
-			tx.commit();
-		}
-		catch( HibernateException e) {
-			e.printStackTrace();
-		}
+	catch( HibernateException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	public void remove()
-	{
-		removeByClass( "Teacher");
-		removeByClass( "Course");
-		removeByClass( "Student");
-	}
+    public void remove()
+    {
+	removeByClass( "Teacher");
+	removeByClass( "Course");
+	removeByClass( "Student");
+    }
 
-	private SessionFactory sessionFactory;
-	private Session session;
-	private Transaction transaction;
+    private SessionFactory sessionFactory;
+    private Session session;
+    private Transaction transaction;
 }
 
