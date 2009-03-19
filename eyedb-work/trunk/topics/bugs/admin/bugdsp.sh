7ab3e49c3c2dd154ee3d942051bf2fe54435635b
eviara <eviara@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2009-03-19 19:13:36 +0000

    Minor changes.

--- eyedb-work/trunk/topics/bugs/admin/bugdsp.sh
+++ eyedb-work/trunk/topics/bugs/admin/bugdsp.sh
@@ -3,7 +3,7 @@
 # bugdsp.sh
 #
 
-db=foo3
+db=foo4
 
 eyedb_dbdelete $db
 
@@ -12,7 +12,10 @@ eyedb_dbcreate $db
 eyedbadmin dataspace list $db
 eyedbadmin datafile create --size=200 $db ${db}02.dat
 eyedbadmin datafile create --size=200 $db ${db}03.dat
-eyedbadmin dataspace update $db 0 1 2
+eyedbadmin datafile create --size=200 $db ${db}04.dat
+eyedbadmin datafile create --size=200 $db ${db}05.dat
+eyedbadmin dataspace create $db DSP_2 1
+eyedbadmin dataspace update $db DEFAULT 2 --migrate-orphan=DSP_2
 
 # this command or any command failed !
 eyedboql -d $db -w
