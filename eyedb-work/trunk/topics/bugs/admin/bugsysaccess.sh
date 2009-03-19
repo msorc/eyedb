cdab5ce7bca24bca4ef090d158bd022329ee0eb6
eviara <eviara@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2009-03-19 11:30:53 +0000

    Fixed bug scripts.

--- eyedb-work/trunk/topics/bugs/admin/bugsysaccess.sh
+++ eyedb-work/trunk/topics/bugs/admin/bugsysaccess.sh
@@ -6,6 +6,9 @@ eyedbadmin user add $eyedbuser $eyedbpasswd
 
 # This commands exits with error:
 #set user system access error: invalid database access mode 0x700
-eyedbadmin $eyedbuser sysaccess toto dbcreate+adduser+deleteuser
+eyedbadmin user sysaccess $eyedbuser dbcreate+adduser+deleteuser
 
 # See DBM_Database.cc line 996 #define check_sysaccess(s, mode)
+
+# fixed 2009-03-19 by EV: dbcreate+adduser+deleteuser no granted anymore by eyedbadmin (help has been updated)
+
