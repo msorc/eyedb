1e60a6183c6618dd371048d2089ab74e568783ad
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2009-05-04 19:23:59 +0000

    First working JSP for creating objects

--- eyedb/trunk/examples/JavaBinding/J2EE/src/org/eyedb/example/servlet/CreatePersonServlet.java
+++ eyedb/trunk/examples/JavaBinding/J2EE/src/org/eyedb/example/servlet/CreatePersonServlet.java
@@ -1,33 +1,80 @@
 package org.eyedb.example.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.eyedb.RecMode;
+
+import person.Person;
 
 /**
  * Servlet implementation class for Servlet: CreatePersonServlet
  *
  */
- public class CreatePersonServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+public class CreatePersonServlet extends EyeDBServlet implements javax.servlet.Servlet {
     /* (non-Java-doc)
-	 * @see javax.servlet.http.HttpServlet#HttpServlet()
-	 */
-	public CreatePersonServlet() {
-		super();
-	}   	
-	
-	/* (non-Java-doc)
-	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}  	
-	
-	/* (non-Java-doc)
-	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}   	  	    
+     * @see javax.servlet.http.HttpServlet#HttpServlet()
+     */
+    public CreatePersonServlet() {
+	super();
+    }   	
+
+    public void init() throws ServletException
+    {
+	super.init();
+
+	try {
+	    person.Database.init();
+	}
+	catch( org.eyedb.Exception e) {
+	    throw new ServletException( e);
+	}
+    }
+
+    private void doCreatePerson( String firstname, String lastname) throws org.eyedb.Exception
+    {
+	getDatabase().transactionBegin();
+
+	Person p = new Person( getDatabase());
+
+	p.setFirstname( firstname);
+	p.setLastname( lastname);
+
+	p.store( RecMode.FullRecurs);
+
+	getDatabase().transactionCommit();
+    }
+
+    /* (non-Java-doc)
+     * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+    }  	
+
+    /* (non-Java-doc)
+     * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String firstname = request.getParameter( "firstname");
+	String lastname = request.getParameter( "lastname");
+
+	if (firstname != null && !firstname.isEmpty()) {
+	    try {
+		openDatabase();
+
+		doCreatePerson( firstname, lastname);
+
+		closeDatabase();
+	    }
+	    catch( org.eyedb.Exception e) {
+		throw new ServletException( e);
+	    }
+	}
+
+	response.sendRedirect("/eyedb/QueryServlet");
+    }   	  	    
 }
