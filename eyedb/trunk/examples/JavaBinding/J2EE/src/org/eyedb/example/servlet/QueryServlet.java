04cf1d6f73e64fb15ecdfd2e6a087a7508e01892
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2009-05-05 10:57:07 +0000

    updated J2EE project

--- eyedb/trunk/examples/JavaBinding/J2EE/src/org/eyedb/example/servlet/QueryServlet.java
+++ eyedb/trunk/examples/JavaBinding/J2EE/src/org/eyedb/example/servlet/QueryServlet.java
@@ -21,85 +21,85 @@ import org.eyedb.Root;
  *
  */
 public class QueryServlet extends EyeDBServlet implements javax.servlet.Servlet {
-    /* (non-Java-doc)
-     * @see javax.servlet.http.HttpServlet#HttpServlet()
-     */
-    public QueryServlet() 
-    {
-	super();
-    }   	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public QueryServlet() 
+	{
+		super();
+	}   	
 
-    private void doOQLQuery( String query, PrintWriter out) throws org.eyedb.Exception
-    {
-	getDatabase().transactionBegin();
+	private void doOQLQuery( String query, PrintWriter out) throws org.eyedb.Exception
+	{
+		getDatabase().transactionBegin();
 
-	OQL q = new org.eyedb.OQL( getDatabase(), query);
-	ObjectArray obj_arr = new ObjectArray();
-	q.execute(obj_arr, RecMode.FullRecurs);
+		OQL q = new org.eyedb.OQL( getDatabase(), query);
+		ObjectArray obj_arr = new ObjectArray();
+		q.execute(obj_arr, RecMode.FullRecurs);
 
-	out.println( "<p>Query result (" + obj_arr.getCount() + " objects):<br>");
-	out.println( "<table border=\"1\"");
-	out.println( "<tr>");
-	out.println("<th>#</th>");
-	out.println("<th>Object</th>");
-	out.println( "</tr>");
-	
-	ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	PrintStream ps = new PrintStream( bos);
-	
-	for (int i = 0; i < obj_arr.getCount(); i++) {
-	    out.println( "<tr>");
-	    out.println("<td>Object " + i + "</td>");
-	    bos.reset();
-	    obj_arr.getObject(i).trace(ps);
-	    out.println("<td><pre>" + bos.toString() + "</pre></td>");
-	    out.println( "</tr>");
-	}
-	out.println( "</table>");
-	out.println( "</p>");
-	
-	getDatabase().transactionCommit();
-    }
+		out.println( "<p>Query result (" + obj_arr.getCount() + " objects):<br>");
+		out.println( "<table border=\"1\"");
+		out.println( "<tr>");
+		out.println("<th>#</th>");
+		out.println("<th>Object</th>");
+		out.println( "</tr>");
 
-    /* (non-Java-doc)
-     * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-    {
-	PrintWriter out = response.getWriter();
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream( bos);
 
-	out.println( "<html>");
-	out.println( "<body>");
+		for (int i = 0; i < obj_arr.getCount(); i++) {
+			out.println( "<tr>");
+			out.println("<td>Object " + i + "</td>");
+			bos.reset();
+			obj_arr.getObject(i).trace(ps);
+			out.println("<td><pre>" + bos.toString() + "</pre></td>");
+			out.println( "</tr>");
+		}
+		out.println( "</table>");
+		out.println( "</p>");
 
-	out.println( "<form name=\"query\" action=\"/eyedb/QueryServlet\" method=\"get\">");
-	out.println( "OQL query: <input type=\"text\" name=\"query\">");
-	out.println( "<input type=\"submit\" value=\"Ok\">");
-	out.println( "</form>");
-
-	String query = request.getParameter( "query");
-	
-	if (query != null && !query.isEmpty()) {
-	    try {
-		openDatabase();
-
-		doOQLQuery( query, out);
-
-		closeDatabase();
-	    }
-	    catch( org.eyedb.Exception e) {
-		throw new ServletException( e);
-	    }
+		getDatabase().transactionCommit();
 	}
 
-	out.println( "</body>");
-	out.println( "</html>");
-    }  	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		PrintWriter out = response.getWriter();
 
-    /* (non-Java-doc)
-     * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-    {
-	// TODO Auto-generated method stub
-    }   	  	    
+		out.println( "<html>");
+		out.println( "<body>");
+
+		out.println( "<form name=\"query\" action=\"/eyedb/QueryServlet\" method=\"get\">");
+		out.println( "OQL query: <input type=\"text\" name=\"query\">");
+		out.println( "<input type=\"submit\" value=\"Ok\">");
+		out.println( "</form>");
+
+		String query = request.getParameter( "query");
+
+		if (query != null && !query.isEmpty()) {
+			try {
+				openDatabase();
+
+				doOQLQuery( query, out);
+
+				closeDatabase();
+			}
+			catch( org.eyedb.Exception e) {
+				throw new ServletException( e);
+			}
+		}
+
+		out.println( "</body>");
+		out.println( "</html>");
+	}  	
+
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+	}   	  	    
 }
