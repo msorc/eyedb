42ea4853a4c4b4290739f9f16eb5b0511eb8b983
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2009-07-03 18:27:45 +0000

    added testcases for OQL triggers bugs

--- eyedb/trunk/tests/bugs/odl-remove-schema/test_odl_remove_schema.py
+++
@@ -1,7 +0,0 @@
-import os
-from eyedb.test.command import run_simple_command
-
-database='bugs_odl_remove_schema'
-
-command="%s/eyedbodl -u -d %s --rmsch" % (os.environ['bindir'], database)
-run_simple_command( command)
