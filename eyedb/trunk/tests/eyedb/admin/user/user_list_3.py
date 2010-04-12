df3da4586b30052e3ad13b899e9e4c2fd6538393
fdechelle <fdechelle@1eaed59b-6812-0410-b3ab-d7fef4b73c88>
2010-04-12 15:18:40 +0000

    fixing compilation problems + failing tests

--- eyedb/trunk/tests/eyedb/admin/user/user_list_3.py
+++ eyedb/trunk/tests/eyedb/admin/user/user_list_3.py
@@ -7,7 +7,7 @@ command="%s/eyedbadmin user list" % (os.environ['bindir'],)
 child = pexpect.spawn( command)
 child.logfile = sys.stdout
 child.expect("name .*: \"%s\"\r\n" % (username,))
-child.expect("sysaccess .*: SET_USER_PASSWD_SYSACCESS_MODE\r\n")
+child.expect("sysaccess .*: SETUSERPASSWORD_SYSACCESS_MODE\r\n")
 child.expect(pexpect.EOF)
 child.close()
 sys.exit(child.exitstatus)
